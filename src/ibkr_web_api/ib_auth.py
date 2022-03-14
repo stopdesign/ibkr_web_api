b1e1d1daa24c3cad46e8c3a305cbcf492968f088
Mikhail Makeev <mihail.makeev@gmail.com>
2022-03-14 14:20:15 +0200

    portal and websocket url

--- src/ibkr_web_api/ib_auth.py
+++ src/ibkr_web_api/ib_auth.py
@@ -76,6 +76,12 @@ class IbApi:
 
         self.base_url = 'https://%s' % self.base_hostname
 
+    def get_portal_url(self):
+        "https://%s/portal.proxy/v1/portal" % self.base_hostname
+
+    def get_websocket_url(self):
+        return "wss://%s/portal.proxy/v1/portal/ws" % self.base_hostname
+
     @property
     def xxx_password(self):
         return "x" * len(self.password)
