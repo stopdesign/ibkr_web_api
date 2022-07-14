977502c80ab722b6cdf0b65ee5d177f84b722d5b
Gregory <gregory@stopdesign.ru>
2022-07-14 13:56:02 -0700

    versions

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import setup
 
 setup(
     name="ibkr_web_api",
-    version="1.1.0",
+    version="1.1.1",
     url="https://github.com/stopdesign/ibkr_web_api.git",
     author="Gregory",
     description="IBKR Web API",
     packages=["ibkr_web_api"],
     package_dir={"": "src"},
     install_requires=[
-        "requests==2.28.0",
-        "termcolor==1.1.0",
-        "pycryptodome==3.14.1",
-        "cryptography==37.0.2",
+        "redis~=4.3",
+        "requests~=2.28",
+        "termcolor~=1.1",
+        "pycryptodome~=3.14",
+        "cryptography~=37.0",
     ],
 )
