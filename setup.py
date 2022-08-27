5b0aa6f6ed64d63da66ed5f31f5a7a6c0f0ce632
Gregory <gregory@stopdesign.ru>
2022-08-27 11:13:52 -0700

    midnight break

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ibkr_web_api",
-    version="1.1.2",
+    version="1.1.3",
     url="https://github.com/stopdesign/ibkr_web_api.git",
     author="Gregory",
     description="IBKR Web API",
--- src/ibkr_web_api/client.py
+++ src/ibkr_web_api/client.py
@@ -92,25 +92,31 @@ class IBThinClient:
     def ibkr_long_break(self, dt=None):
         """
         Долгий перерыв с полной перезагрузкой.
+        Ничего не работает. После перерыва требуется обновление сессии.
 
         Friday, 20:00 - 23:59 US/Pacific
         """
         dt = dt or datetime.utcnow().replace(tzinfo=pytz.utc)
         dt = dt.astimezone(tz=pytz.timezone("US/Pacific"))
 
-        return dt.isoweekday() == 5 and dt.time() >= time(20, 00)
+        return dt.isoweekday() == 5 and dt.time() >= time(20, 0)
 
     def ibkr_short_break(self, dt=None):
         """
         Короткий перерыв с легкой перезагрузкой.
+        Сервисы по очереди отключаются на несколько минут.
         Сессия не сбрасывается.
 
-        Saturday - Thursday, 20:45 - 21:45 US/Pacific
+        Sat - Thu, 20:45 - 21:45 US/Pacific
+        Every day, 00:00 - 00:03 US/Pacific
         """
         dt = dt or datetime.utcnow().replace(tzinfo=pytz.utc)
         dt = dt.astimezone(tz=pytz.timezone("US/Pacific"))
 
-        return dt.isoweekday() != 5 and (time(20, 45) <= dt.time() <= time(21, 45))
+        reboot = dt.isoweekday() != 5 and (time(20, 45) <= dt.time() <= time(21, 45))
+        midnight = time(0, 0) <= dt.time() <= time(0, 3)
+
+        return reboot or midnight
 
 class IBClient(IBThinClient):
     """
