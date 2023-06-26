466a4b81dcb25dfe61833e7dc454c36360cea770
Gregory <gregory@stopdesign.ru>
2023-06-25 19:59:12 -0700

    versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ibkr_web_api",
-    version="1.1.3",
+    version="1.1.4",
     url="https://github.com/stopdesign/ibkr_web_api.git",
     author="Gregory",
     description="IBKR Web API",
@@ -12,7 +12,7 @@ setup(
         "redis~=4.3",
         "requests~=2.28",
         "termcolor~=1.1",
-        "pycryptodome~=3.14",
-        "cryptography~=37.0",
+        "pycryptodome~=3.18",
+        "cryptography~=41.0",
     ],
 )
